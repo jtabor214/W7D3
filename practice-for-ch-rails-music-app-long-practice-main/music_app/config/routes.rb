33a19af15d557737685ce73aeb7e7cce224b5866
Jacob Tabor <jtabor214@gmail.com>
2023-11-29 15:12:30 -0500

    Created UsersController, finished #index, #show, #new, #create

--- practice-for-ch-rails-music-app-long-practice-main/music_app/config/routes.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :users, only: [:index, :show, :new, :create]
+  resource :session, only: [:new, :create, :destroy]
 end
