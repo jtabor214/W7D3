f95204e0ce53e58415cc20f4420a4c56f5dc6c81
Jacob Tabor <jtabor214@gmail.com>
2023-11-29 16:23:37 -0500

    Added #require_logged_out to ApplicationController ; Created SessionsController and completed #new

--- practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/application_controller.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/application_controller.rb
@@ -21,6 +21,10 @@ class ApplicationController < ActionController::Base
         redirect_to new_session_url unless logged_in?
     end 
 
+    def require_logged_out 
+        redirect_to users_url if logged_in?
+    end
+
     def logout!
         @current_user.reset_session_token! if logged_in?
         session[:session_token] = nil 
--- practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/sessions_controller.rb
@@ -1,5 +1,14 @@
 class SessionsController < ApplicationController
 
-    
+    before_action :logged_in?
+
+    def new 
+        @user=User.new
+        render :new
+    end
+
+    def destroy 
+        
+    end
 
 end
