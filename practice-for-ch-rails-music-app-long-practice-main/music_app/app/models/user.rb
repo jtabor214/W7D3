22f8959583686373a2905516476de63fc507afa2
Yaqi Fang <cfang322@gmail.com>
2023-11-29 14:40:24 -0500

    Finish User Model, #ensure_session_token, #reset_session_token::find_by_credentials

--- practice-for-ch-rails-music-app-long-practice-main/music_app/app/models/user.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ApplicationRecord
-    #FIGVAPBER
+    #FIGVAPEBR
+    #find_by credentials/is_password?/generate_session_token/validates/attr_reader/password=/ensure_session_token/before_validation/reset_session_token!
     before_validation :ensure_session_token
 
     validates :email, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true 
     validates :password, length: {minimum: 6, allow_nil: true}
+
+    def self.find_by_credentials(email, password) 
+        @user = User.find_by(email: email)
+        if @user && @user.is_password?(password)
+            @user
+        else
+            nil 
+        end 
+    end 
 
     attr_reader :password 
 
@@ -18,4 +28,18 @@ class User < ApplicationRecord
         password_obj.is_password?(password)
     end
     
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+        # self.session_token ||= generate_unique_session_token <line33-35>
+    end 
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end 
+
+    # def generate_unique_session_token
+    #     SecureRandom::urlsafe_base64
+    # end 
 end
