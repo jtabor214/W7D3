684ddf931e367b6a221f6eaa3b800903c2e4f583
Jacob Tabor <jtabor214@gmail.com>
2023-11-29 14:22:37 -0500

    Completed #password=, #is_password, validations for model Users, before_validations callback, and #password

--- practice-for-ch-rails-music-app-long-practice-main/music_app/app/models/user.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+    #FIGVAPBER
+    before_validation :ensure_session_token
+
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true 
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    attr_reader :password 
+
+    def password=(password)
+        @password = password 
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        password_obj = BCrypt::Password.new(self.password_digest)
+        password_obj.is_password?(password)
+    end
+    
 end
